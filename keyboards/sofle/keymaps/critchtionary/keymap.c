754355f3e849b809f8a492368ee1f239c06d199c
Tom <12222103+critchtionary@users.noreply.github.com>
2024-04-07 16:03:48 +0100

    Fix left encoder being flipped

--- keyboards/sofle/keymaps/critchtionary/keymap.c
+++ keyboards/sofle/keymaps/critchtionary/keymap.c
@@ -192,3 +192,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
+
+#ifdef ENCODER_ENABLE
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        // Left encoder is backwards
+        if (clockwise) {
+            tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
+        }
+        return false;
+    }
+    return true;
+}
+
+#endif
