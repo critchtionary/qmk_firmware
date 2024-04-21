ca671e9f2cb1b1a41ed7f0b33a2372b0c7878cf3
Tom <12222103+critchtionary@users.noreply.github.com>
2024-04-21 15:55:05 +0100

    Fix mac commands (#4)
    * Fix mac commands
    
    * Add key to make window full screen (on mac)
    
    * Add command for locking
    
    * Add reverse search key
    
    * Add pound key

--- keyboards/sofle/keymaps/critchtionary/keymap.c
+++ keyboards/sofle/keymaps/critchtionary/keymap.c
@@ -20,7 +20,11 @@ enum custom_keycodes {
     KC_WINUP,
     KC_WINDN,
     KC_WINLF,
-    KC_WINRT
+    KC_WINRT,
+    KC_WINFL,
+    KC_LOCK,
+    KC_RSRCH,
+    KC_GBP,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -41,11 +45,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */ 
 
 [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_QUOT,
-  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_RSFT,
-  KC_GRV,   KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,   XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
-                 KC_LGUI, KC_LALT, KC_LCTL, TL_LOWR, KC_SPC,      KC_BSPC, TL_UPPR, KC_RCTL, KC_RALT, KC_RGUI
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_QUOT,
+  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_RSFT,
+  KC_GRV,   KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
+                  KC_LGUI, KC_LALT, KC_LCTL, TL_LOWR, KC_SPC,    KC_BSPC, TL_UPPR, KC_RCTL, KC_RALT, KC_RGUI
 ),
 /* SYMBOLS
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -55,25 +59,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LShift|  !   |  _   |   '  |   (  |   )  |-------.    ,-------|   =  |   4  |   5  |   6  |   #  |RShift|
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * |  `   |  ^   |  |   |   :  |   {  |   }  |-------|    |-------|   0  |   1  |   2  |   3  |   \  | Enter|
+ * |  £   |  ^   |  |   |   :  |   {  |   }  |-------|    |-------|   0  |   1  |   2  |   3  |   \  | Enter|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LGUI | LAlt | LCTR |LOWER | /Space  /       \ Bspc \  |RAISE | RCTR | RAlt | RGUI |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `-----------------------------------'           '------''---------------------------'
  */
-
 [_SYMBOLS] = LAYOUT(
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7, KC_F8, KC_F9, KC_F10,  KC_F11,
   _______, KC_TILD, KC_MINS, KC_DQUO, KC_LBRC, KC_RBRC,                         KC_PLUS, KC_7,  KC_8,  KC_9,  KC_ASTR, KC_F12,
   _______, KC_EXLM, KC_UNDS, KC_QUOT, KC_LPRN, KC_RPRN,                         KC_EQL,  KC_4,  KC_5,  KC_6,  KC_HASH, _______,
-  _______, KC_CIRC, KC_PIPE, KC_COLN, KC_LCBR, KC_RCBR, _______,       _______, KC_0,    KC_1,  KC_2,  KC_3,  KC_NUBS, _______,
-                       _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
+  KC_GBP,  KC_CIRC, KC_PIPE, KC_COLN, KC_LCBR, KC_RCBR, _______,       _______, KC_0,    KC_1,  KC_2,  KC_3,  KC_BSLS, _______,
+                    _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 /* COMMANDS
  * ,----------------------------------------.                     ,-----------------------------------------.
  * | ESC  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Caps | Ins  | Pscr | Menu |      |      |                    | PgUp | PWrd |  Up  | NWrd | DLine| F12  |
+ * | Caps | Ins  | Pscr | Menu |RSrch |      |                    | PgUp | PWrd |  Up  | NWrd | DLine| F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LShift|      | Snip |      |      |      |-------.    ,-------| PgDn | Left | Down | Right|  Del |RShift|
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
@@ -84,17 +87,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `-----------------------------------'           '------''---------------------------'
  */
 [_COMMANDS] = LAYOUT(
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                            KC_F6,   KC_F7,    KC_F8,   KC_F9,    KC_F10,     KC_F11,
-  KC_CAPS, KC_INS,  KC_PSCR, KC_APP,  XXXXXXX, XXXXXXX,                          KC_PGUP, KC_PRVWD, KC_UP,   KC_NXTWD, C(KC_BSPC), KC_F12,
-  _______, XXXXXXX, KC_SNIP, XXXXXXX, XXXXXXX, XXXXXXX,                          KC_PGDN, KC_LEFT,  KC_DOWN, KC_RGHT,  KC_DEL,     _______,
-  _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX, _______,       _______, XXXXXXX,  KC_HOME, XXXXXXX,  KC_END,  XXXXXXX,    _______,
-                         _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,                           KC_F6,   KC_F7,    KC_F8,    KC_F9,    KC_F10,     KC_F11,
+  KC_CAPS, KC_INS,  KC_PSCR, KC_APP,  KC_RSRCH, XXXXXXX,                         KC_PGUP, KC_PRVWD, KC_UP,    KC_NXTWD, C(KC_BSPC), KC_F12,
+  _______, XXXXXXX, KC_SNIP, XXXXXXX, XXXXXXX,  XXXXXXX,                         KC_PGDN, KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_DEL,     _______,
+  _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V),  XXXXXXX, _______,       _______, XXXXXXX, KC_HOME,  XXXXXXX,  KC_END,   XXXXXXX,    _______,
+                     _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * | Lock |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |QK_BOOT|     |QWERTY|      |      |      |                    |      |      | WinUp|      |      |      |
+ * |QK_BOOT|     |QWERTY|      |      |      |                    |      |WinFl | WinUp|      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |MACWIN|      |      |      |-------.    ,-------|      |WinLf | WinDn| WinRt|      |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
@@ -105,11 +108,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `-----------------------------------'           '------''---------------------------'
  */
   [_ADJUST] = LAYOUT(
-  XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,
-  QK_BOOT, XXXXXXX, KC_QWERTY, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX,  KC_WINUP, XXXXXXX,  XXXXXXX, XXXXXXX,
+  KC_LOCK, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,
+  QK_BOOT, XXXXXXX, KC_QWERTY, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_WINFL, KC_WINUP, XXXXXXX,  XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, CG_TOGG,   XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_WINLF, KC_WINDN, KC_WINRT, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                   _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
+  XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,
+                      _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
   )
 };
 
@@ -117,8 +120,8 @@ void move_window(uint16_t direction, bool pressed) {
     if (pressed) {
         if (keymap_config.swap_lctl_lgui) {
             // Mac
+            register_mods(mod_config(MOD_LALT));
             register_mods(mod_config(MOD_LGUI));
-            register_mods(mod_config(MOD_LCTL));
             register_code(direction);
         } else {
             // Windows
@@ -127,8 +130,8 @@ void move_window(uint16_t direction, bool pressed) {
         }
     } else {
         if (keymap_config.swap_lctl_lgui) {
+            unregister_mods(mod_config(MOD_LALT));
             unregister_mods(mod_config(MOD_LGUI));
-            unregister_mods(mod_config(MOD_LCTL));
             unregister_code(direction);
         } else {
             unregister_mods(mod_config(MOD_LGUI));
@@ -190,7 +193,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             if (record->event.pressed) {
                 if (keymap_config.swap_lctl_lgui) {
                     // Mac
-                    register_mods(mod_config(MOD_LGUI));
+                    register_mods(mod_config(MOD_LCTL));
                     register_mods(mod_config(MOD_LSFT));
                     register_code(KC_4);
                 } else {
@@ -201,7 +204,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             } else {
                 if (keymap_config.swap_lctl_lgui) {
-                    unregister_mods(mod_config(MOD_LGUI));
+                    unregister_mods(mod_config(MOD_LCTL));
                     unregister_mods(mod_config(MOD_LSFT));
                     unregister_code(KC_4);
                 } else {
@@ -222,6 +225,65 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
         case KC_WINRT:
             move_window(KC_RIGHT, record->event.pressed);
+            break;
+        case KC_WINFL:
+            move_window(KC_ENT, record->event.pressed);
+            break;
+        case KC_LOCK:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                    // Mac
+                    register_mods(mod_config(MOD_LCTL));
+                    register_mods(mod_config(MOD_LGUI));
+                    register_code(KC_Q);
+                } else {
+                    // Windows
+                    register_mods(mod_config(MOD_LGUI));
+                    register_code(KC_L);
+                }
+            } else {
+                if (keymap_config.swap_lctl_lgui) {
+                    unregister_mods(mod_config(MOD_LCTL));
+                    unregister_mods(mod_config(MOD_LGUI));
+                    unregister_code(KC_Q);
+                } else {
+                    unregister_mods(mod_config(MOD_LGUI));
+                    unregister_code(KC_L);
+                }
+            }
+            break;
+        case KC_RSRCH:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                    // Mac
+                    register_mods(mod_config(MOD_LGUI));
+                    register_code(KC_R);
+                } else {
+                    // Windows
+                    register_mods(mod_config(MOD_LCTL));
+                    register_code(KC_R);
+                }
+            } else {
+                if (keymap_config.swap_lctl_lgui) {
+                    unregister_mods(mod_config(MOD_LGUI));
+                    unregister_code(KC_R);
+                } else {
+                    unregister_mods(mod_config(MOD_LCTL));
+                    unregister_code(KC_R);
+                }
+            }
+            break;
+        case KC_GBP:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                    // Mac
+                    set_unicode_input_mode(UNICODE_MODE_MACOS);
+                } else {
+                    // Windows
+                    set_unicode_input_mode(UNICODE_MODE_WINDOWS);
+                }
+                send_unicode_string("£");
+            }
             break;
     }
     return true;
