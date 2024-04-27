a24cd0d86d2aae934a09bc1d93745b1114f8de62
Tom <12222103+critchtionary@users.noreply.github.com>
2024-04-27 16:23:59 +0100

    Swap p and semi colon keys (#6)

--- keyboards/sofle/keymaps/critchtionary/keymap.c
+++ keyboards/sofle/keymaps/critchtionary/keymap.c
@@ -5,8 +5,8 @@
 #include "oled.c"
 
 enum sofle_layers {
-    _QWERTY,
-    _QWERTY_MAC,
+    _JKLP,
+    _JKLP_MAC,
 
     _COMMANDS,
     _SYMBOLS,
@@ -15,8 +15,8 @@ enum sofle_layers {
 };
 
 enum custom_keycodes {
-    KC_QWERTY = QK_USER,
-    KC_QWERTY_MAC,
+    KC_JKLP = QK_USER,
+    KC_JKLP_MAC,
     KC_PRVWD,
     KC_NXTWD,
     KC_SNIP,
@@ -31,13 +31,13 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
- * QWERTY
+ * JKLP
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  '   |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   ;  |  '   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |RShift|
+ * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   P  |RShift|
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * | LAlt |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |Enter |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -46,21 +46,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `-----------------------------------'           '------''---------------------------'
  */ 
 
-[_QWERTY] = LAYOUT(
+[_JKLP] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_QUOT,
-  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_RSFT,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,   KC_SCLN,    KC_QUOT,
+  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,   KC_P, KC_RSFT,
   KC_LALT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
                   KC_LGUI, KC_LALT, KC_LCTL, TL_LOWR, KC_SPC,    KC_BSPC, TL_UPPR, KC_RCTL, KC_RALT, KC_RGUI
 ),
 /*
- * QWERTY_MAC
+ * JKLP_MAC
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  '   |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   ;  |  '   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |RShift|
+ * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   P  |RShift|
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * | LCTR |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |Enter |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -69,10 +69,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `-----------------------------------'           '------''---------------------------'
  */ 
 
-[_QWERTY_MAC] = LAYOUT(
+[_JKLP_MAC] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_QUOT,
-  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_RSFT,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,   KC_SCLN,    KC_QUOT,
+  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,   KC_P, KC_RSFT,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
                   KC_LOPT, KC_LCTL, KC_LCMD, TL_LOWR, KC_SPC,    KC_BSPC, TL_UPPR, KC_RCMD, KC_RCTL, KC_ROPT
 ),
@@ -133,17 +133,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `-----------------------------------'           '------''---------------------------'
  */
   [_ADJUST] = LAYOUT(
-  KC_LOCK, KC_QWERTY, KC_QWERTY_MAC, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,
-  QK_BOOT, XXXXXXX,   XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_WINFL, KC_WINUP, XXXXXXX,  XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX,   XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_WINLF, KC_WINDN, KC_WINRT, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX,   XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,
+  KC_LOCK, KC_JKLP, KC_JKLP_MAC,   XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,
+  QK_BOOT, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_WINFL, KC_WINUP, XXXXXXX,  XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_WINLF, KC_WINDN, KC_WINRT, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,
                       _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
   )
 };
 
 void move_window(uint16_t direction, bool pressed) {
     if (pressed) {
-        if (get_highest_layer(default_layer_state) == _QWERTY_MAC) {
+        if (get_highest_layer(default_layer_state) == _JKLP_MAC) {
             // Mac
             register_mods(mod_config(MOD_LALT));
             register_mods(mod_config(MOD_LCTL));
@@ -154,7 +154,7 @@ void move_window(uint16_t direction, bool pressed) {
             register_code(direction);
         }
     } else {
-        if (get_highest_layer(default_layer_state) == _QWERTY_MAC) {
+        if (get_highest_layer(default_layer_state) == _JKLP_MAC) {
             unregister_mods(mod_config(MOD_LALT));
             unregister_mods(mod_config(MOD_LCTL));
             unregister_code(direction);
@@ -167,19 +167,19 @@ void move_window(uint16_t direction, bool pressed) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case KC_QWERTY:
+        case KC_JKLP:
             if (record->event.pressed) {
-                set_single_persistent_default_layer(_QWERTY);
+                set_single_persistent_default_layer(_JKLP);
             }
             return false;
-        case KC_QWERTY_MAC:
+        case KC_JKLP_MAC:
             if (record->event.pressed) {
-                set_single_persistent_default_layer(_QWERTY_MAC);
+                set_single_persistent_default_layer(_JKLP_MAC);
             }
             return false;
         case KC_PRVWD:
             if (record->event.pressed) {
-                if (get_highest_layer(default_layer_state) == _QWERTY_MAC) {
+                if (get_highest_layer(default_layer_state) == _JKLP_MAC) {
                     // Mac
                     register_mods(mod_config(MOD_LALT));
                 } else {
@@ -188,7 +188,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
                 register_code(KC_LEFT);
             } else {
-                if (get_highest_layer(default_layer_state) == _QWERTY_MAC) {
+                if (get_highest_layer(default_layer_state) == _JKLP_MAC) {
                     unregister_mods(mod_config(MOD_LALT));
                 } else {
                     unregister_mods(mod_config(MOD_LCTL));
@@ -198,7 +198,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
         case KC_NXTWD:
              if (record->event.pressed) {
-                if (get_highest_layer(default_layer_state) == _QWERTY_MAC) {
+                if (get_highest_layer(default_layer_state) == _JKLP_MAC) {
                     // Mac
                     register_mods(mod_config(MOD_LALT));
                 } else {
@@ -207,7 +207,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
                 register_code(KC_RIGHT);
             } else {
-                if (get_highest_layer(default_layer_state) == _QWERTY_MAC) {
+                if (get_highest_layer(default_layer_state) == _JKLP_MAC) {
                     unregister_mods(mod_config(MOD_LALT));
                 } else {
                     unregister_mods(mod_config(MOD_LCTL));
@@ -219,7 +219,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             if (record->event.pressed) {
                 register_mods(mod_config(MOD_LGUI));
                 register_mods(mod_config(MOD_LSFT));
-                if (get_highest_layer(default_layer_state) == _QWERTY_MAC) {
+                if (get_highest_layer(default_layer_state) == _JKLP_MAC) {
                     // Mac
                     register_code(KC_4);
                 } else {
@@ -229,7 +229,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             } else {
                 unregister_mods(mod_config(MOD_LGUI));
                 unregister_mods(mod_config(MOD_LSFT));
-                if (get_highest_layer(default_layer_state) == _QWERTY_MAC) {
+                if (get_highest_layer(default_layer_state) == _JKLP_MAC) {
                     unregister_code(KC_4);
                 } else {
                     unregister_code(KC_S);
@@ -253,7 +253,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
         case KC_LOCK:
             if (record->event.pressed) {
-                if (get_highest_layer(default_layer_state) == _QWERTY_MAC) {
+                if (get_highest_layer(default_layer_state) == _JKLP_MAC) {
                     // Mac
                     register_mods(mod_config(MOD_LCTL));
                     register_mods(mod_config(MOD_LGUI));
@@ -264,7 +264,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     register_code(KC_L);
                 }
             } else {
-                if (get_highest_layer(default_layer_state) == _QWERTY_MAC) {
+                if (get_highest_layer(default_layer_state) == _JKLP_MAC) {
                     unregister_mods(mod_config(MOD_LCTL));
                     unregister_mods(mod_config(MOD_LGUI));
                     unregister_code(KC_Q);
@@ -276,7 +276,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
         case KC_GBP:
             if (record->event.pressed) {
-                if (get_highest_layer(default_layer_state) == _QWERTY_MAC) {
+                if (get_highest_layer(default_layer_state) == _JKLP_MAC) {
                     // Mac
                     register_mods(mod_config(MOD_LALT));
                     register_code(KC_3);
@@ -286,7 +286,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     send_unicode_string("£");
                 }
             } else {
-                if (get_highest_layer(default_layer_state) == _QWERTY_MAC) {
+                if (get_highest_layer(default_layer_state) == _JKLP_MAC) {
                     unregister_mods(mod_config(MOD_LALT));
                     unregister_code(KC_3);
                 }
