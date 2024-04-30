1723df6c3ceb1b663060a8fa68c5fbc247621e21
Tom <12222103+critchtionary@users.noreply.github.com>
2024-04-30 10:19:21 +0100

    Move some symbols around

--- keyboards/sofle/keymaps/critchtionary/keymap.c
+++ keyboards/sofle/keymaps/critchtionary/keymap.c
@@ -80,11 +80,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | ESC  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |  ~   |  -   |   "  |   [  |   ]  |                    |   +  |   7  |   8  |   9  |   *  | F12  |
+ * | Tab  |  |   |  -   |   "  |   [  |   ]  |                    |   +  |   7  |   8  |   9  |   *  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  `   |  !   |  _   |   '  |   (  |   )  |-------.    ,-------|   =  |   4  |   5  |   6  |   #  |  £   |
+ * |  $   |  @   |  _   |   '  |   (  |   )  |-------.    ,-------|   =  |   4  |   5  |   6  |   #  |  £   |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |  ^   |  @   |  |   |   :  |   {  |   }  |-------|    |-------|   0  |   1  |   2  |   3  |   \  | Enter|
+ * |  ^   |  !   |  ~   |   `  |   {  |   }  |-------|    |-------|   0  |   1  |   2  |   3  |   \  | Enter|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            |      |      |      |LOWER | /Space  /       \ Bspc \  |RAISE |      |      |      |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
@@ -92,9 +92,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_SYMBOLS] = LAYOUT(
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7, KC_F8, KC_F9, KC_F10,  KC_F11,
-  _______, KC_TILD, KC_MINS, KC_DQUO, KC_LBRC, KC_RBRC,                         KC_PLUS, KC_7,  KC_8,  KC_9,  KC_ASTR, KC_F12,
-  KC_GRV,  KC_EXLM, KC_UNDS, KC_QUOT, KC_LPRN, KC_RPRN,                         KC_EQL,  KC_4,  KC_5,  KC_6,  KC_HASH, KC_GBP,
-  KC_CIRC, KC_AT,   KC_PIPE, KC_COLN, KC_LCBR, KC_RCBR, _______,       _______, KC_0,    KC_1,  KC_2,  KC_3,  KC_BSLS, _______,
+  _______, KC_PIPE, KC_MINS, KC_DQUO, KC_LBRC, KC_RBRC,                         KC_PLUS, KC_7,  KC_8,  KC_9,  KC_ASTR, KC_F12,
+  KC_DLR,  KC_AT,   KC_UNDS, KC_QUOT, KC_LPRN, KC_RPRN,                         KC_EQL,  KC_4,  KC_5,  KC_6,  KC_HASH, KC_GBP,
+  KC_CIRC, KC_EXLM, KC_TILD, KC_GRV , KC_LCBR, KC_RCBR, _______,       _______, KC_0,    KC_1,  KC_2,  KC_3,  KC_BSLS, _______,
                     _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 /* COMMANDS
